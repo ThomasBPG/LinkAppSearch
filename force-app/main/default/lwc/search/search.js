e9ce018c8d2484a50fe97a95779644bd82460a9c
Thomas Braad Perret-Gentil <github@magicx.dk>
2024-10-27 14:42:19 +0100

    readthis tag now standard selected.

--- force-app/main/default/lwc/search/search.js
+++ force-app/main/default/lwc/search/search.js
@@ -8,17 +8,26 @@ export default class Search extends LightningElement {
     @track searchResult;
     @track error;
     tags;
+    linkTags;
 
     runSearch(event) {
         
         if(event.keyCode == 13)
         {
             this.searchTerm = event.target.value;
-            if(this.searchTerm!=null)
+            if(this.searchTerm!=null && this.searchTerm.length>0)
             {
                 doSearch({queryText: this.searchTerm})
                 .then(links =>
                 {
+                    /*
+                    // Add "Edit"-link to each of the found links.
+                    for (var i=0; i<this.links.length; i++)
+                    {
+                        this.searchResult = [...this.searchResult, {editLink: 'hest'}];
+                    }
+                    */
+                    
                     this.searchResult = links;
                     this.error = undefined;
                 })
@@ -36,11 +45,11 @@ export default class Search extends LightningElement {
         getTagsFromLinkIds({linkIds: linkIds})
         .then(links =>
         {
-            this.tags = links;
+            for(var i=0; i<links.length; i++)  {
+                //this.tagItems = [...this.tagItems ,{value: data[i].Id , label: data[i].Name} ];
+                this.linkTags = [...this.linkTags ,{value: links[i].Id, label: links[i].Name}];
+            }
 
-            // Do some magic with a Map or associative array here...
-
-            this.error = undefined;
         })
         .catch(error =>
         {
